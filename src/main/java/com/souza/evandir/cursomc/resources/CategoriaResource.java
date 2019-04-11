f25d0e38912ae809a7d9473fb649cc5ec925e593
Evandir <evandirs@yahoo.com.br>
2019-04-10 22:44:44 -0300

    Criando Repository e Service para Categoria

--- src/main/java/com/souza/evandir/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/souza/evandir/cursomc/resources/CategoriaResource.java
@@ -3,15 +3,30 @@ package com.souza.evandir.cursomc.resources;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.souza.evandir.cursomc.domain.Categoria;
+import com.souza.evandir.cursomc.services.CategoriaService;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
+	
+	@Autowired
+	CategoriaService service;
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+		
+		Categoria obj = service.buscar(id);
+				
+		return ResponseEntity.ok().body(obj);
+	}
 	
 	@RequestMapping(method=RequestMethod.GET)
 	public List<Categoria> listar() {
---
+++ src/main/java/com/souza/evandir/cursomc/services/CategoriaService.java
@@ -0,0 +1,22 @@
+package com.souza.evandir.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.souza.evandir.cursomc.domain.Categoria;
+import com.souza.evandir.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+	}
+	
+}
