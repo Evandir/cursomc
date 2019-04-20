4de8ce4a55e87bce44d901c8eb38b167f38fc68d
Evandir <evandirs@yahoo.com.br>
2019-04-20 12:49:48 -0300

    Ajustes no endpoint /Categorias/{id}

--- src/main/java/com/souza/evandir/cursomc/services/CategoriaService.java
+++ src/main/java/com/souza/evandir/cursomc/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.souza.evandir.cursomc.domain.Categoria;
 import com.souza.evandir.cursomc.repositories.CategoriaRepository;
+import com.souza.evandir.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,7 +17,7 @@ public class CategoriaService {
 	
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 	
 }
