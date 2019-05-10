0c0b95cd8828f23164fbba059895df77b459b612
Evandir <evandirs@yahoo.com.br>
2019-05-09 22:08:57 -0300

    Deletando uma Categoria com DELETE

--- src/main/java/com/souza/evandir/cursomc/services/CategoriaService.java
+++ src/main/java/com/souza/evandir/cursomc/services/CategoriaService.java
@@ -4,10 +4,12 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.souza.evandir.cursomc.domain.Categoria;
 import com.souza.evandir.cursomc.repositories.CategoriaRepository;
+import com.souza.evandir.cursomc.services.exceptions.DataIntegrityException;
 import com.souza.evandir.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -33,5 +35,15 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
+		}
 	}
 }
